f902d8d1df48d8921b914277b8b7e27e2f92e09b
Stanley Nguyen <hung.ngn.the@gmail.com>
2019-12-26 16:00:11 +0800

    chore(card example) Adjust card example without tokenization

--- examples/card.js
+++ examples/card.js
@@ -1,58 +1,22 @@
 const x = require('./xendit');
-const readline = require('readline');
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
 
 const Card = x.Card;
 const card = new Card({});
 
+// These IDs should be obtained using Xendit.js
+// https://docs.xendit.co/xenpayments/payments-credit-cards-overview/credit-cards-integration-and-testing/collecting-card-details-tokenization/index.html
+const tokenID = '5e0461a86113354249aab7ec';
+const authID = '5e0461a96113354249aab7ee';
+
 card
-  .createToken({
-    cardNumber: '4000000000000002',
-    expMonth: '12',
-    expYear: '2020',
-    cardCVN: '123',
-    isSingleUse: false,
-    shouldAuthenticate: false,
+  .createCharge({
+    tokenID,
+    authID,
+    amount: 10000,
+    // eslint-disable-next-line max-len
+    externalID: Date.now().toString(), // use your system's ID of the transaction
+    capture: false,
   })
-  .then(({ id }) => {
-    return Promise.all([
-      card.createAuthentication({
-        tokenID: id,
-        amount: 10000,
-      }),
-      { tokenID: id },
-    ]);
-  })
-  .then(r => {
-    console.log('tokenization response:', r); // eslint-disable-line no-console
-    return r;
-  })
-  .then(([{ id, status, payer_authentication_url }, { tokenID }]) => {
-    if (status === Card.Status.VERIFIED) {
-      resolve({ authID: id, tokenID });
-    } else if (status === Card.Status.IN_REVIEW) {
-      return new Promise(resolve => {
-        rl.question(
-          `Please go to this URL ${payer_authentication_url} to authenticate. Once done, press enter to continue to create charge`, // eslint-disable-line max-len
-          () => resolve({ authID: id, tokenID }),
-        );
-      });
-    }
-  })
-  .then(({ authID, tokenID }) =>
-    card.createCharge({
-      tokenID,
-      authID,
-      amount: 10000,
-      // eslint-disable-next-line max-len
-      externalID: Date.now().toString(), // use your system's ID of the transaction
-      capture: false,
-    }),
-  )
   .then(r => {
     console.log('charge created:', r); // eslint-disable-line no-console
     return r;
