5504f4b0539c6711e100dc7e368b83428ef034aa
Ervan <vaanz.adetya@gmail.com>
2019-12-26 17:04:35 +0700

    Add dana.d.ts

---
+++ src/ewallet/dana.d.ts
@@ -0,0 +1,9 @@
+export function createPayment(data: {
+  externalID: string;
+  amount: number;
+  expirationDate: string;
+  callbackURL: string;
+  redirectURL: string;  
+}): Promise<object>;
+export function getByExtID(data: { externalID: string }): Promise<object>;
+  
