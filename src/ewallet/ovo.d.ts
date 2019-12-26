13bf214161bf17332d20a433fcf0b522f4bbc956
Ervan <vaanz.adetya@gmail.com>
2019-12-26 17:00:01 +0700

    Add OVO.d.ts

---
+++ src/ewallet/ovo.d.ts
@@ -0,0 +1,6 @@
+export function createPayment(data: {
+  externalID: string;
+  amount: number;
+  phone: string;
+}): Promise<object>;
+export function getByExtID(data: { externalID: string }): Promise<object>;
