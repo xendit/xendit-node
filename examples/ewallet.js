440a53c4be7e64a9e902d9e05d67e6ccaa1a3390
Ervan <vaanz.adetya@gmail.com>
2019-12-28 15:15:01 +0700

    Add linkAja example

--- examples/ewallet.js
+++ examples/ewallet.js
@@ -45,3 +45,33 @@ ew.createDanaPayment({
     console.error(e); // eslint-disable-line no-console
     process.exit(1);
   });
+
+ew.createLinkAjaPayment({
+  externalID: new Date(),
+  phone: '081234567890',
+  amount: 30000,
+  items: [
+    {
+      id: '123123',
+      name: 'Phone Case',
+      price: 100000,
+      quantity: 1,
+    },
+    {
+      id: '345678',
+      name: 'Powerbank',
+      price: 200000,
+      quantity: 1,
+    },
+  ],
+  callbackURL: 'https://yourwebsite.com/callback',
+  redirectURL: 'https://yourwebsite.com/callback',
+})
+  .then(r => {
+    console.log('create linkAja payment detail:', r); // eslint-disable-line no-console
+    return r;
+  })
+  .catch(e => {
+    console.error(e); // eslint-disable-line no-console
+    process.exit(1);
+  });
