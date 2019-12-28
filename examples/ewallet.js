45f8866d4d02a59b60e446415e0789fb64332d36
Ervan <vaanz.adetya@gmail.com>
2019-12-28 15:14:25 +0700

    Add dana example

--- examples/ewallet.js
+++ examples/ewallet.js
@@ -6,17 +6,39 @@ const ew = new EWallet({});
 ew.createOVOPayment({
   externalID: new Date(),
   amount: 1,
-  phone: '087877971875',
+  phone: '081234567890',
 })
   .then(r => {
-    console.log('create ovo payment detail:', r); // eslint-disable-line no-console
+    console.log('create OVO payment detail:', r); // eslint-disable-line no-console
     return r;
   })
   .then(({ external_id }) =>
     ew.getOVOPaymentStatusByExtID({ externalID: external_id }),
   )
   .then(r => {
-    console.log('ovo payment status detail:', r); // eslint-disable-line no-console
+    console.log('OVO payment status detail:', r); // eslint-disable-line no-console
+    return r;
+  })
+  .catch(e => {
+    console.error(e); // eslint-disable-line no-console
+    process.exit(1);
+  });
+
+ew.createDanaPayment({
+  externalID: new Date(),
+  amount: 1,
+  callbackURL: 'http://google.com',
+  redirectURL: 'http://google.com',
+})
+  .then(r => {
+    console.log('create dana payment detail:', r); // eslint-disable-line no-console
+    return r;
+  })
+  .then(({ external_id }) =>
+    ew.getDanaPaymentStatusByExtID({ externalID: external_id }),
+  )
+  .then(r => {
+    console.log('Dana payment status detail:', r); // eslint-disable-line no-console
     return r;
   })
   .catch(e => {
