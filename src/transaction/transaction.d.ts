211549dc6cf40329cb68eb05b5dd6669d751bed3
MESMUR <msmaahirur@gmail.com>
2022-02-02 12:01:28 +0530

    FIX: list transaction options, and .nvmrc EOL

--- src/transaction/transaction.d.ts
+++ src/transaction/transaction.d.ts
@@ -1,46 +1,5 @@
 import { XenditOptions } from '../xendit_opts';
 
-// enum transactionTypes {
-//   disbursement = 'DISBURSEMENT',
-//   batchDisbursement = 'BATCH_DISBURSEMENT',
-//   payment = 'PAYMENT',
-//   remittance = 'REMITTANCE',
-//   remittancePayout = 'REMITTANCE_PAYOUT',
-//   transfer = 'TRANSFER',
-//   platformFee = 'PLATFORM_FEE',
-//   refund = 'REFUND',
-//   other = 'OTHER',
-// }
-
-// enum statusTypes {
-//   pending = 'PENDING',
-//   success = 'SUCCESS',
-//   failed = 'FAILED',
-//   voided = 'VOIDED',
-//   reversed = 'REVERSED',
-// }
-
-// enum channelTypes {
-//   bank = 'BANK',
-//   cash = 'CASH',
-//   cards = 'CARDS',
-//   cardlessCredit = 'CARDLESS_CREDIT',
-//   directDebit = 'DIRECT_DEBIT',
-//   eWallet = 'EWALLET',
-//   payLater = 'PAYLATER',
-//   qrCode = 'QR_CODE',
-//   retailOutlet = 'RETAIL_OUTLET',
-//   virtualAccount = 'VIRTUAL_ACCOUNT',
-// }
-
-// enum currencyTypes {
-//   IDR = 'IDR',
-//   PHP = 'PHP',
-//   USD = 'USD',
-//   SGD = 'SGD',
-//   MYR = 'MYR',
-// }
-
 export = class Transaction {
   constructor({});
   static _constructorWithInjectedXenditOpts: (
