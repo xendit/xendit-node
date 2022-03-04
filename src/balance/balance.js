60a5c91b56e71bd772cb610e14aa9bd4d0c3f172
Viktor U <viktor@xendit.co>
2022-03-04 12:11:38 +0200

    refactor balance api

--- src/balance/balance.js
+++ src/balance/balance.js
@@ -1,5 +1,4 @@
 const {
-  promWithJsErr,
   Auth,
   fetchWithHTTPErr,
   queryStringWithoutUndefined,
@@ -7,36 +6,32 @@ const {
 
 const BALANCE_PATH = '/balance';
 
-function Balance(options) {
-  let aggOpts = options;
-  if (Balance._injectedOpts && Object.keys(Balance._injectedOpts).length > 0) {
-    aggOpts = Object.assign({}, options, Balance._injectedOpts);
+class Balance {
+  static _injectedOpts = {};
+
+  static _constructorWithInjectedXenditOpts(options) {
+    this._injectedOpts = options;
+    return this;
   }
 
-  this.opts = aggOpts;
-  this.API_ENDPOINT = this.opts.xenditURL + BALANCE_PATH;
-}
+  static AccountType = {
+    Cash: 'CASH',
+    Holding: 'HOLDING',
+    Tax: 'TAX',
+  };
 
-Balance._injectedOpts = {};
-Balance._constructorWithInjectedXenditOpts = function(options) {
-  Balance._injectedOpts = options;
-  return Balance;
-};
+  constructor(options) {
+    this.opts = { ...options, ...Balance._injectedOpts };
+    this.API_ENDPOINT = this.opts.xenditURL + BALANCE_PATH;
+  }
 
-Balance.AccountType = {
-  Cash: 'CASH',
-  Holding: 'HOLDING',
-  Tax: 'TAX',
-};
-
-Balance.prototype.getBalance = function(data) {
-  return promWithJsErr((resolve, reject) => {
+  async getBalance(data) {
     const queryStr = data
       ? queryStringWithoutUndefined({ account_type: data.accountType })
       : '';
     const queryStrWithQuestionMark = queryStr ? `?${queryStr}` : '';
 
-    let headers = {
+    const headers = {
       Authorization: Auth.basicAuthHeader(this.opts.secretKey),
     };
 
@@ -44,13 +39,11 @@ Balance.prototype.getBalance = function(data) {
       headers['for-user-id'] = data.forUserID;
     }
 
-    fetchWithHTTPErr(`${this.API_ENDPOINT}${queryStrWithQuestionMark}`, {
+    return fetchWithHTTPErr(`${this.API_ENDPOINT}${queryStrWithQuestionMark}`, {
       method: 'GET',
       headers,
-    })
-      .then(resolve)
-      .catch(reject);
-  });
-};
+    });
+  }
+}
 
 module.exports = Balance;
